f30657adb472f24945bb25a0a96429b0ece9eebb
Romulo Carneiro <romulo.carneirodk@gmail.com>
2022-11-04 15:36:37 -0300

    Setup mobile

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from 'native-base'
+
+const SignIn = () => {
+    return (
+        <Center flex={1} bgColor="gray.900">
+            <Text color="white" fontSize="xl" fontFamily="heading">
+                Sign In
+            </Text>
+        </Center>
+    )
+}
+export default SignIn
